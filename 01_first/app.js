f682484a1c238b575742c0b24b1142e6edc6dfb6
neeleshbizoara <bizoaraneelesh@gmail.com>
2024-10-12 18:53:14 +0530

    JSX is HLML like syntax which compiled in to React Element and those react element render on DOM using reactDOM.

--- 01_first/app.js
+++ 01_first/app.js
@@ -4,24 +4,64 @@
 // root.render(heading);
 import React from "react";
 import ReactDOM from "react-dom";
-const heading1 = React.createElement('div', { id: 'parent' },
-    [
-        React.createElement('div', { id: 'child' },
-            [
-                React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 1 '),
-                React.createElement('h2', { style: { color: 'gray', fontStyle: 'italic' } }, 'Small Hello World 1')
-            ]
-        ),
-        React.createElement('div', { id: 'child2' },
-            [
-                React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 2'),
-                React.createElement('h2', { style: { color: 'gray', fontStyle: 'italic' } }, 'Small Hello World 2')
-            ]
-        )
-    ]
-)
 
-console.log(heading1);
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading1);
+const heading = React.createElement(
+  "h1",
+  { style: { color: "red" } },
+  "Hi Again."
+);
+console.log(heading);
+// JSX is not HTML
+
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hi world using JSX
+  </h1>
+);
+console.log(jsxHeading);
+
+const expressionConst = 1000;
+const rootElemet = ReactDOM.createRoot(document.getElementById("root"));
+rootElemet.render(heading);
+
+const HeadingComponent = () => <h1>Heading Functional component</h1>;
+const FunctionalComponent = function () {
+    return <p>Functional component written using normal Function way.</p>
+} 
+const HeadingComponent1 = () => {
+  return (
+    <div>
+          <h1 className="head">Heading Functional component</h1>
+          <FunctionalComponent />
+          {expressionConst} {100 + 200 }
+          <HeadingComponent/>
+    </div>
+  );
+};
+
+// rootElemet.render(jsxHeading);
+rootElemet.render(<HeadingComponent1/>)
+
+// const heading1 = React.createElement('div', { id: 'parent' },
+//     [
+//         React.createElement('div', { id: 'child' },
+//             [
+//                 React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 1 '),
+//                 React.createElement('h2', { style: { color: 'gray', fontStyle: 'italic' } }, 'Small Hello World 1')
+//             ]
+//         ),
+//         React.createElement('div', { id: 'child2' },
+//             [
+//                 React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 2'),
+//                 React.createElement('h2', { style: { color: 'gray', fontStyle: 'italic' } }, 'Small Hello World 2')
+//             ]
+//         )
+//     ]
+// )
+
+// console.log(heading1);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading1);
