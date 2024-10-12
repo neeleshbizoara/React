6316fc668e7ce588a9ad7cc65dd12386a5de39cd
neeleshbizoara <bizoaraneelesh@gmail.com>
2024-10-12 10:53:29 +0530

    parcel, react, react-dom added in package and impotered in js file

--- 01_first/app.js
+++ 01_first/app.js
@@ -2,12 +2,13 @@
 // console.log(heading); //Object or React Element
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
-
+import React from "react";
+import ReactDOM from "react-dom";
 const heading1 = React.createElement('div', { id: 'parent' },
     [
         React.createElement('div', { id: 'child' },
             [
-                React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 1'),
+                React.createElement('h1', { style: { color: 'green', fontStyle: 'italic' } }, 'Hello World 1 '),
                 React.createElement('h2', { style: { color: 'gray', fontStyle: 'italic' } }, 'Small Hello World 1')
             ]
         ),
